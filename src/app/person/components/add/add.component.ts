2a9a2b210b9d6399a2ed7ece46315d753daf83f1
jhonc <calderonjhondairo@gmail.com>
2024-07-05 17:25:35 -0500

    form validators

--- src/app/person/components/add/add.component.ts
+++ src/app/person/components/add/add.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { FormBuilder, FormGroup, Validators as v } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  Validators as v,
+} from '@angular/forms';
 import { Person } from '../../interfaces/person';
 import { Router } from '@angular/router';
 import { RestService } from '../../services/rest.service';
@@ -17,13 +23,7 @@ export class AddComponent implements OnInit {
   hide = signal(true);
   statePerson?: Person;
 
-  clickEvent(event: MouseEvent) {
-    this.hide.set(!this.hide);
-    event.stopPropagation();
-  }
-
   form!: FormGroup;
-  errorMessage: string = '';
   minDate = new Date();
   maxDate = new Date();
 
@@ -40,30 +40,56 @@ export class AddComponent implements OnInit {
   }
 
   ngOnInit() {
+    let namesValid = [
+      v.required,
+      v.minLength(3),
+      v.pattern('^[a-zA-ZÀ-ÿ\\s]+$'),
+    ];
     this.defineDates();
-    this.form = this.fb.group({
-      id: [this.statePerson?.id || 0],
-      name: [this.statePerson?.name || '', [v.required]],
-      lastName: [this.statePerson?.lastName || '', [v.required]],
-      email: [this.statePerson?.email || '', [v.required, v.email]],
-      phone: [this.statePerson?.phone || '', [v.required]],
-      dob: [
-        this.statePerson ? new Date(this.statePerson.birthDate) : '',
-        [v.required],
-      ],
-      docType: [
-        this.statePerson?.document.type.toLowerCase() || 'cc',
-        [v.required],
-      ],
-      docNumber: [this.statePerson?.document.number || '', [v.required]],
-      docDescription: [this.statePerson?.document.description || ''],
-      city: [this.statePerson?.city.name || '', [v.required]],
-      state: [this.statePerson?.city.state || '', [v.required]],
-      country: [this.statePerson?.city.country || '', [v.required]],
-      username: [this.statePerson?.username || '', [v.required]],
-      password: [this.statePerson?.password || '', [v.required]],
-      confirmPassword: [this.statePerson?.password || '', [v.required]],
-    });
+    this.form = this.fb.group(
+      {
+        id: [this.statePerson?.id || 0],
+        name: [this.statePerson?.name || '', namesValid],
+        lastName: [this.statePerson?.lastName || '', namesValid],
+        email: [
+          this.statePerson?.email || '',
+          [v.required, v.pattern('^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{1,}$')],
+        ],
+        phone: [
+          this.statePerson?.phone || '',
+          [v.required, v.pattern('^\\+?[0-9]{7,15}$')],
+        ],
+        dob: [
+          this.statePerson ? new Date(this.statePerson.birthDate) : '',
+          [v.required],
+        ],
+        docType: [
+          this.statePerson?.document.type.toLowerCase() || 'cc',
+          [v.required],
+        ],
+        docNumber: [
+          this.statePerson?.document.number || '',
+          [v.required, v.minLength(7)],
+        ],
+        docDescription: [this.statePerson?.document.description || ''],
+        city: [this.statePerson?.city.name || '', namesValid],
+        state: [this.statePerson?.city.state || '', namesValid],
+        country: [this.statePerson?.city.country || '', namesValid],
+        username: [
+          this.statePerson?.username || '',
+          v.pattern('^[a-zA-Z0-9._-]{5,20}$'),
+        ],
+        password: [
+          this.statePerson?.password || '',
+          [
+            v.required,
+            v.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$'),
+          ],
+        ],
+        confirmPassword: [this.statePerson?.password || '', [v.required]],
+      },
+      { validators: matchPasswordValidator('password', 'confirmPassword') },
+    );
 
     if (this.statePerson) {
       this.form.get('password')!.disable();
@@ -77,19 +103,6 @@ export class AddComponent implements OnInit {
     this.minDate.setFullYear(current.getFullYear() - 100);
   }
 
-  updateErrorMessage() {
-    const emailControl = this.form.get('email');
-    if (emailControl!.invalid) {
-      if (emailControl!.hasError('required')) {
-        this.errorMessage = 'Email is required';
-      } else if (emailControl!.hasError('email')) {
-        this.errorMessage = 'Invalid email format';
-      }
-    } else {
-      this.errorMessage = '';
-    }
-  }
-
   preventDefault(event: Event) {
     event.preventDefault();
   }
@@ -101,13 +114,14 @@ export class AddComponent implements OnInit {
 
   onSave() {
     let person: Person = this.form.getRawValue();
+    if (!this.form.valid) return;
     of(this.service.put(person)).subscribe({
       next: (response) =>
         response.subscribe((msg) => {
           this.openSnackBar(msg);
         }),
     });
-    // this.save(person, this.statePerson ? this.service.put : this.service.post);
+
   }
 
   save(person: Person, saveMethod: saveMethod) {
@@ -125,4 +139,39 @@ export class AddComponent implements OnInit {
       verticalPosition: 'top',
     });
   }
+
+  showHint(key: string) {
+    let control = this.form.get(key);
+    return control?.dirty && control?.invalid && control.value;
+    // return this.form.get(key)?.dirty && this.form.get(key)?.invalid;
+  }
+}
+
+export function matchPasswordValidator(
+  password: string,
+  confirmPassword: string,
+): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    const passwordControl = control.get(password);
+    const confirmPasswordControl = control.get(confirmPassword);
+
+    if (!passwordControl || !confirmPasswordControl) {
+      return null;
+    }
+
+    if (
+      confirmPasswordControl.errors &&
+      !confirmPasswordControl.errors['passwordMismatch']
+    ) {
+      return null;
+    }
+
+    if (passwordControl.value !== confirmPasswordControl.value) {
+      confirmPasswordControl.setErrors({ passwordMismatch: true });
+    } else {
+      confirmPasswordControl.setErrors(null);
+    }
+
+    return null;
+  };
 }
